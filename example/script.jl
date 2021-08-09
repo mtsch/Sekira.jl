021003a1ed5339a66f807b30ecd02e38bc07100b
mtsch <matijacufar@gmail.com>
2021-08-09 15:22:23 +1200

    Clean up example

--- example/script.jl
+++ example/script.jl
@@ -6,8 +6,6 @@
 # or see
 # julia> ?Sekira.plateau
 using Sekira
-#H = HubbardMom1D(BoseFS((0, 0, 0, 7, 0, 0, 0)); u=6)
-#H = HubbardMom1D(BoseFS((0, 0, 0, 0, 10, 0, 0, 0, 0, 0)); u=6)
 add = BoseFS(ntuple(i -> ifelse(i==10, 10, 0), 20))
 H = HubbardMom1D(add; u=0.5)
 
